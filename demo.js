a6455d85e1ff4de42966038f8f94eb3cf58f062c
Leslie Guo <35063609+Leslie-ZJU@users.noreply.github.com>
2019-05-26 10:43:59 +0800

    add react feature

---
+++ demo.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+    el: '#shop',
+    data: {
+      shop_name: 'BLLIZARD',
+      seller_name: 'NETEASE',
+      message: 'HearthStone/炉石 经典卡包',
+      properties: [
+        { text: '经典卡包：60' },
+        { text: '打开卡包收获快乐' },
+        { text: '每包至少包含一张稀有品质卡牌' }
+      ],
+      price: 388,
+      status: '正在配送中',
+      J_Exclude: {
+        display: 'none'
+    }
+    },
+  })
